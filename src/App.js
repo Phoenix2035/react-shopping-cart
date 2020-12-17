2ddd925b524cb01e987e23bcfd44a0c33cfc331a
masturdating1991 <aligodosi@gmail.com>
2020-12-17 20:19:56 +0330

    done

--- src/App.js
+++ src/App.js
@@ -1,89 +1,91 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Cart from './components/Cart';
 import Filter from './components/Filter';
 import Products from './components/Products';
 import data from './data.json';
 
+
+
 function App() {
-    const [products, setProducts] = useState(data.products)
-    const [size, setSize] = useState("")
-    const [sort, setSort] = useState("")
-    const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem('cartItems')) ? JSON.parse(localStorage.getItem('cartItems')) : [])
+    const [products, setProducts] = useState(data.products);
+    const [size, setSize] = useState("");
+    const [sort, setSort] = useState("");
+    const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem('cartItems')) ? JSON.parse(localStorage.getItem('cartItems')) : []);
 
     const createOrder = order => {
         alert("Need to save order for " + order.name)
-    }
+    };
 
     const removeFromCart = product => {
-        const carts = cartItems.slice()
-        setCartItems(carts.filter(item => item._id !== product._id))
+        const carts = cartItems.slice();
+        setCartItems(carts.filter(item => item._id !== product._id));
         localStorage.setItem("cartItems", JSON.stringify(carts.filter(item => item._id !== product._id)))
-    }
+    };
 
     const addToCart = product => {
-        let alreadyInCart = false
-        const carts = cartItems.slice()
+        let alreadyInCart = false;
+        const carts = cartItems.slice();
 
         carts.forEach(item => {
             if (item._id === product._id) {
-                item.count++
+                item.count++;
                 alreadyInCart = true
             }
-        })
+        });
         if (!alreadyInCart) {
-            carts.push({ ...product, count: 1 })
+            carts.push({...product, count: 1})
         }
-        setCartItems(carts)
+        setCartItems(carts);
 
         localStorage.setItem("cartItems", JSON.stringify(carts))
-    }
+    };
 
 
     const filterProducts = (e) => {
         if (e.target.value === '') {
-            setSize(e.target.value)
+            setSize(e.target.value);
             setProducts(data.products)
         } else {
-            setSize(e.target.value)
+            setSize(e.target.value);
             setProducts(data.products.filter(item => item.availableSizes.indexOf(e.target.value) >= 0))
         }
+    };
 
-    }
     const sortProducts = (e) => {
-        const sortVal = e.target.value
-        setSort(sortVal)
+        const sortVal = e.target.value;
+        setSort(sortVal);
         setProducts((products) => products.slice().sort((a, b) => (
             sortVal === "lowest" ? a.price > b.price ? 1 : -1 :
                 sortVal === 'highest' ? a.price < b.price ? 1 : -1 : a._id > b._id ? 1 : -1
         )))
-    }
+    };
 
     return (
-        <div className="grid-container">
-            <header>
-                <a href="/">React Shopping Cart</a>
-            </header>
-            <main>
-                <div className="content">
-                    <div className="main">
-                        <Filter
-                            count={products.length}
-                            size={size}
-                            sort={sort}
-                            filterProducts={filterProducts}
-                            sortProducts={sortProducts}
-                        />
-                        <Products products={products} addToCart={addToCart} />
+            <div className="grid-container">
+                <header>
+                    <a href="/">React Shopping Cart</a>
+                </header>
+                <main>
+                    <div className="content">
+                        <div className="main">
+                            <Filter
+                                count={products.length}
+                                size={size}
+                                sort={sort}
+                                filterProducts={filterProducts}
+                                sortProducts={sortProducts}
+                            />
+                            <Products products={products} addToCart={addToCart}/>
+                        </div>
+                        <div className="sidebar">
+                            <Cart cartItems={cartItems} removeFromCart={removeFromCart} addOrder={createOrder}/>
+                        </div>
                     </div>
-                    <div className="sidebar">
-                        <Cart cartItems={cartItems} removeFromCart={removeFromCart} addOrder={createOrder} />
-                    </div>
-                </div>
-            </main>
-            <footer>
-                All right is reserved
-            </footer>
-        </div>
+                </main>
+                <footer>
+                    All right is reserved
+                </footer>
+            </div>
     );
 }
 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,9 +4,10 @@ import Fade from 'react-reveal/Fade';
 import Zoom from "react-reveal/Zoom";
 import Modal from 'react-modal';
 
-function Products({products, addToCart}) {
+function Products(props) {
 
     const [modalProduct, setModalProduct] = useState(null);
+
 
     const openModal = product => {
         setModalProduct({product})
@@ -20,36 +21,43 @@ function Products({products, addToCart}) {
     return (
         <div>
             <Fade bottom cascade>
-                <ul className="products">
-                    {
-                        products.map(product =>
-                            <li key={product._id}>
-                                <div className="product">
-                                    <a href={"#" + product._id}>
-                                        <img
-                                            onClick={() => openModal(product)}
-                                            src={product.image}
-                                            alt={product.title}/>
-                                        <p>
-                                            {product.title}
-                                        </p>
-                                    </a>
-                                    <div className="product-price">
-                                        <div>
-                                            {formatCurrency(product.price)}
-                                        </div>
+                {
+                    !props.products
+                        ?
+                        <div>Loading...</div>
+                        :
+                        <ul className="products">
+                            {
+                                props.products.map(product =>
+                                    <li key={product._id}>
+                                        <div className="product">
+                                            <a href={"#" + product._id}>
+                                                <img
+                                                    onClick={() => openModal(product)}
+                                                    src={product.image}
+                                                    alt={product.title}/>
+                                                <p>
+                                                    {product.title}
+                                                </p>
+                                            </a>
+                                            <div className="product-price">
+                                                <div>
+                                                    {formatCurrency(product.price)}
+                                                </div>
 
-                                        <button
-                                            onClick={() => addToCart(product)}
-                                            className="button primary">
-                                            Add To Cart
-                                        </button>
-                                    </div>
-                                </div>
-                            </li>
-                        )
-                    }
-                </ul>
+                                                <button
+                                                    onClick={() => props.addToCart(product)}
+                                                    className="button primary">
+                                                    Add To Cart
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </li>
+                                )
+                            }
+                        </ul>
+                }
+
             </Fade>
             {
                 modalProduct && (
@@ -81,7 +89,7 @@ function Products({products, addToCart}) {
                                             {formatCurrency(modalProduct.product.price)}
                                         </div>
                                         <button className="button primary" onClick={() => {
-                                            addToCart(modalProduct.product);
+                                            props.addToCart(modalProduct.product);
                                             closeModal();
                                         }}>
                                             Add To Cart
